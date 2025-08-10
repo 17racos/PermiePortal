dd1646c82adb103bfd1f7b3b7e7340144e783f33
17racos <17racos@gmail.com>
2025-08-10 14:51:03 +0000

    fix(encoding): wrap pages in BaseLayout with <meta charset>, unify head markup

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
-["src/content/projects/ditch-food-forest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fditch-food-forest.mdx&astroContentModuleFlag=true")],
-["src/content/projects/mangrove-micro-nursery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmangrove-micro-nursery.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/guerrilla-irrigation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fguerrilla-irrigation.mdx&astroContentModuleFlag=true")],
+["src/content/articles/historys-greatest-environmentalist.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhistorys-greatest-environmentalist.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/lawn-apocalypse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flawn-apocalypse.mdx&astroContentModuleFlag=true")],
-["src/content/articles/historys-greatest-environmentalist.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhistorys-greatest-environmentalist.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/ditch-food-forest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fditch-food-forest.mdx&astroContentModuleFlag=true")],
+["src/content/projects/mangrove-micro-nursery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmangrove-micro-nursery.mdx&astroContentModuleFlag=true")]]);
 		
