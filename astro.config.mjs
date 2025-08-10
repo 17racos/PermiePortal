c735321d6d9a5326fb58bb0441e27cc431e010dd
17racos <17racos@gmail.com>
2025-08-10 13:09:26 +0000

    fix(routing): use entry.slug references; set output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: true }), mdx()],
   server: { host: true, port: 4321 },
   markdown: { smartypants: true },
+  output: 'static',
 });
