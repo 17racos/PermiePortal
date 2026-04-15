cb48a6ee0d7ab3a0d25dcddd166c8906abe8e9ee
17racos <17racos@gmail.com>
2026-04-15 18:58:34 +0000

    Adding Duckweed Articles

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -13,5 +13,7 @@ export default new Map([
 ["src/content/projects/mangrove-micro-nursery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmangrove-micro-nursery.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/ditch-food-forest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fditch-food-forest.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/we-saved-the-lake-by-killing-it.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fwe-saved-the-lake-by-killing-it.mdx&astroContentModuleFlag=true")],
-["src/content/articles/duckweed-nature-submitted-a-patch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-nature-submitted-a-patch.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/duckweed-nature-submitted-a-patch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-nature-submitted-a-patch.mdx&astroContentModuleFlag=true")],
+["src/content/articles/duckweed-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-analysis.mdx&astroContentModuleFlag=true")],
+["src/content/articles/duckweed-rotten-article.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-rotten-article.mdx&astroContentModuleFlag=true")]]);
 		
