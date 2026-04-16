d6e628b713ee4ed1ace4ba6cc2b9e56204d5952b
17racos <17racos@gmail.com>
2026-04-16 15:36:51 +0000

    Added Permagolf Article

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -16,5 +16,6 @@ export default new Map([
 ["src/content/articles/duckweed-nature-submitted-a-patch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-nature-submitted-a-patch.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/duckweed-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-analysis.mdx&astroContentModuleFlag=true")],
 ["src/content/articles/duckweed-rotten-article.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fduckweed-rotten-article.mdx&astroContentModuleFlag=true")],
-["src/content/articles/lawn-history.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flawn-history.mdx&astroContentModuleFlag=true")]]);
+["src/content/articles/lawn-history.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Flawn-history.mdx&astroContentModuleFlag=true")],
+["src/content/articles/permagolf.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fpermagolf.mdx&astroContentModuleFlag=true")]]);
 		
