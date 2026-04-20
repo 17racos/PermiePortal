c99fb2fb0367097a9125bae69238b06cc72f6f8a
17racos <17racos@gmail.com>
2026-04-20 10:18:17 +0000

    Fix sitemap.xml -- use plants/pests JSON data instead of broken content collection

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,23 +1,20 @@
-import { getCollection } from 'astro:content';
+import plantsData from '../data/plants.json';
+import pestsData from '../data/pests.json';
 
 export async function GET() {
-  const base = 'https://permieportal.com';
+  const base = 'https://www.permieportal.com';
 
-  const staticPaths = ['/', '/articles', '/projects', '/about'];
+  const staticPaths = ['/', '/plants', '/pests', '/diagnose', '/articles', '/projects', '/about'];
+  const plantPaths = plantsData.map(p => `/plants/${p.slug}`);
+  const pestPaths = pestsData.map(p => `/pests/${p.slug}`);
 
-  const articles = await getCollection('articles');
-  const articlePaths = articles.map(a => `/articles/${a.slug}`);
-
-  const plants = await getCollection('plants');
-  const plantPaths = plants.map(p => `/plants/${p.slug}`);
-
-  const allPaths = [...staticPaths, ...articlePaths, ...plantPaths];
+  const allPaths = [...staticPaths, ...plantPaths, ...pestPaths];
 
   const xml =
     `<?xml version="1.0" encoding="UTF-8"?>\n` +
-    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
-    allPaths.map(p => `<url><loc>${base}${p}</loc></url>`).join('') +
-    `</urlset>`;
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
+    allPaths.map(p => `  <url><loc>${base}${p}</loc></url>`).join('\n') +
+    `\n</urlset>`;
 
   return new Response(xml, { headers: { 'Content-Type': 'application/xml' } });
 }
